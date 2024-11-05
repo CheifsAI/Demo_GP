be36ffe9d68b86221226febc0fde559705d44174
mohamed-hekal17 <hekalmohamed06@gmail.com>
2024-11-05 19:47:06 +0200

    Update SImple.Ex..py

--- Hekal/SImple.Ex..py
+++ Hekal/SImple.Ex..py
@@ -1,20 +1,23 @@
 #Prompt
+from langchain_community.llms import Ollama
 from langchain_core.prompts import PromptTemplate
+from langchain_core.messages import HumanMessage
+
+llm = Ollama(model="llama3.1:8b")
 
 prompt = PromptTemplate.from_template("What is a good name for a company that makes {product}?")
 prompt.format(product="colorful socks")
 
-# difference between an LLM and a ChatModel
 
-from langchain_core.messages import HumanMessage
 
 text = "What would be a good company name for a company that makes colorful socks?"
 messages = [HumanMessage(content=text)]
 
-llm.invoke(text)
+names = llm.invoke(text)
+print(names)
 # >> Feetful of Fun
 
-chat_model.invoke(messages)
+"""chat_model.invoke(messages)
 # >> AIMessage(content="Socks O'Color")
 
 #Output parsers
@@ -22,4 +25,4 @@ from langchain.output_parsers import CommaSeparatedListOutputParser
 
 output_parser = CommaSeparatedListOutputParser()
 output_parser.parse("hi, bye")
-# >> ['hi', 'bye']
+# >> ['hi', 'bye']"""
